6987d725bd3c3ddb5f19e0314f5b15b6fea4fe64
SidMox120897 <120897@unsaac.edu.pe>
2023-02-07 00:20:55 -0500

    mm
    ??

--- src/app/main-page/components/search/search.component.ts
+++ src/app/main-page/components/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/interfaces/alumno';
+import { Docente } from 'src/app/interfaces/docente';
 import { Equipo } from 'src/app/interfaces/equipo';
+import { AlumnoService } from 'src/app/services/alumno.service';
+import { DocenteService } from 'src/app/services/docente.service';
 import { EquipoService } from 'src/app/services/equipo.service';
 
 @Component({
@@ -9,12 +13,39 @@ import { EquipoService } from 'src/app/services/equipo.service';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private equipoServi:EquipoService) { }
-  bodyObject:Equipo[]=[];
+  constructor(
+    private equipoServi:EquipoService,
+    private docServi:DocenteService,
+    private alumnoServi:AlumnoService) { }
+
+  Equipos:Equipo[]=[];
+  Alumnos:Alumno[]=[];
+  Docs:Docente[]=[];
 
   ngOnInit(): void {
     this.equipoServi.getallEquipo().subscribe((res)=>{
-      this.bodyObject=res;
+      this.Equipos=res;
     });
+
+    this.Equipos.forEach(E => {
+      this.docServi.getDocente(E.CodDocente).subscribe((res)=>{
+        if(res.data.length===1){
+          this.Docs.push(res.data);
+          console.log(res.data);
+        }else{
+          this.Docs.push( {} as Docente );
+        }
+      });
+      this.alumnoServi.getAlumno(E.CodAlumnoGuia).subscribe((res)=>{
+        if(res.status===1){
+          this.Alumnos.push(res.respuesta);
+        }else{
+          this.Alumnos.push( {} as Alumno );
+        }
+      });
+    });
+    console.log(this.Alumnos);
+    console.log(this.Docs);
+
   }
 }
